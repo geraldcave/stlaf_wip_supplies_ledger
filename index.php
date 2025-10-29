db4f59e35c1eb6b90f9d6f47d4e3e299fb5ac454
geraldcave <g.cabelin231@gmail.com>
2025-10-29 12:03:20 +0800

    oh

---
+++ index.php
@@ -0,0 +1,34 @@
+<php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>STLAF | WIP SUPPLIES LEDGER</title>
+  <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="icon" type="image/png" href="assets/images/sub_logo_light.png">
+</head>
+
+<body>
+
+  <nav class="navbar px-5">
+    <div class="d-flex align-items-center gap-5">
+      <a class="navbar-brand m-0 p-0" href="index.php">
+        <img src="assets/images/logo_v1_light.png" alt="Logo" width="100" height="80">
+      </a>
+      <h3 class="supply mb-0">WIP Supplies Ledger</h3>
+    </div>
+  </nav>
+
+  
+
+<script src="assets/bootstrap/all.min.js"></script>
+<script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
